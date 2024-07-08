ae7b5968ff3672ab1845ea00848dd76d70c0b32b
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-08 09:41:59 +0200

    Popolo la classe prodotti

---
+++ src/index.php
@@ -0,0 +1,43 @@
+<?php 
+
+include __DIR__ . "/class/prodotti.php";
+
+new prodotti('Royal Canin Mini Adult', 43.99, 545, 'Prosciutto, riso', 'cane');
+new prodotti('Almo Nature Holistic Maintenance Medium Large Tonno e Riso', 44.99, 600, 'Manzo, cereali', 'cane');
+new prodotti('Almo Nature Cat Daily Lattina', 34.99, 400, 'Tonno, pollo, prosciutto', 'gatto');
+new prodotti('Mangime per Pesci Guppy in Fiocchi', 2.95, 30, 'Pesci e sottoprodotti dei pesci, cereali, lieviti, alghe', 'pesce');
+new prodotti('Voliera Wilma in Legno', 184.99, 'Legno', 'M: L 83 x P 67 x H 153 cm', 'uccello');
+new prodotti('Cartucce Filtranti per Filtro EasyCrystal', 2.99, 'Materiale espanso', 'ND', 'pesce');
+new prodotti('Kong Classic', 13.49, 'Galleggia e rimbalza', '8,5cm x 10cm', 'cane');
+new prodotti('Topini di peluche Trixie', 4.99, 'Morbido con codina in corda', '8,5cm x 10cm', 'gatto');
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php-oop-2</title>
+</head>
+<body>
+    <header>
+        <section>
+            <article>
+                <h1>Pet-Shop</h1>
+            </article>
+        </section>
+    </header>
+    <main>
+        <section>
+            <article>
+                <ul>
+                    <li>1</li>
+                </ul>
+            </article>
+        </section>
+    </main>
+    <footer></footer>
+</body>
+</html>
