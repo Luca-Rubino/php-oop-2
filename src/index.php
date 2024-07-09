65e6486ef49013d3799f46db534db6194f8a4348
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-09 11:47:38 +0200

    inserire collecamento al file css

--- src/index.php
+++ src/index.php
@@ -21,6 +21,7 @@ $prodotti = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Php-oop-2</title>
 </head>
 <body>
