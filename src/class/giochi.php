511dd7b069b305d8df360b5589ba5bed02412d97
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-11 19:59:50 +0200

    Creare classe giochi

---
+++ src/class/giochi.php
@@ -0,0 +1,34 @@
+<?php 
+require_once __DIR__ . '/prodotti.php';
+
+class giochi extends prodotti {
+    private $materiali;
+    private $misura;
+
+    // costruttori
+    public function __construct($nome, $prezzo, $categoria, $urlImg, $materiali, $misura,){
+        parent::__construct($nome, $prezzo, $categoria, $urlImg, $materiali, $misura,);
+        $this -> materiali = $materiali;
+        $this -> misura = $misura;
+    }
+
+    // materiali
+    public function getMateriali(){
+        return $this -> materiali;
+    }
+
+    public function setMateriali($materiali){
+        $this -> materiali = $materiali;
+    }
+
+    // misura
+    public function getMisura(){
+        return $this -> misura;
+    }
+
+    public function setMisura($misura){
+        $this -> misura = $misura;
+    }
+}
+
+?>
